1f2a4cac6c12d9d0f003c54317aab0e2c6d4ff1c
Cristhian <cristhiangomezdev@gmail.com>
2022-10-26 18:25:59 +0200

    Arrays

---
+++ main.js
@@ -0,0 +1,31 @@
+//Ejeercicio 1
+let seriesPending = ['Breakin Bug', 'Game of Drones', 'Boolean 99', 'Arrested Devolopment']
+seriesPending.push('Lost')
+//console.log(seriesPending);
+
+//  for (let i = 0; i <seriesPending.length; i++) {
+//     console.log(seriesPending[i], i);
+//     if(seriesPending[i] === 'Boolean 99'){
+//         console.log('Found it!');  
+//     } else{
+        
+//     }
+    
+// }
+
+//Ejercicio 2
+// let seriesWatching = []
+
+
+//  seriesPending.shift()
+// //console.log(seriesPending)
+
+//  seriesWatching.push(seriesPending.shift())
+//  console.log(seriesWatching)
+// console.error('HOLA');
+// for(i=0; i<4; i++){
+//     seriesPending.shift()
+//     seriesWatching.push(seriesPending.shift())
+   
+// }
+// console.log(seriesWatching)
